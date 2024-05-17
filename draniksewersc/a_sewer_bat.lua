6bd3e709fd38bc1e9b1b0b315360c690e84d7d82
twincannon <twincannon@gmail.com>
2024-05-17 19:47:00 -0400

    Roambox tweaks

--- draniksewersc/a_sewer_bat.lua
+++ draniksewersc/a_sewer_bat.lua
@@ -1,3 +1,12 @@
 function event_spawn(e)
-	e.self:SetSimpleRoamBox(100.0)
+	if(math.random(1,100) <= 50) then -- 50% chance
+		eq.set_timer("roambox", math.random(500,5000));
+	end
+end
+
+function event_timer(e)
+	if e.timer == "roambox" then
+		eq.stop_timer("roambox");
+		e.self:SetSimpleRoamBox(25.0)
+	end
 end
--- freportw/Server_Greeter.lua
+++ freportw/Server_Greeter.lua
@@ -1,6 +1,7 @@
+---@param e NPCEventSay
 function event_say(e)
 	if(e.message:findi("hail")) then
-		e.self:Say("Hello and welcome to Everquest Underground! The server is still very WIP so expect bugs and weird things. Visit us on Discord at discord.gg/WswdAAnhS9 for more information. Would you like to hear about our " .. eq.say_link("What features?", false, "features") .. "?")
+		e.self:Say("Hello and welcome to Everquest Underground! The server is still very WIP so expect bugs and weird things. Visit us on Discord at discord.gg/WswdAAnhS9 for more information and server files. Would you like to hear about our " .. eq.say_link("What features?", false, "features") .. "?")
 	elseif(e.message:findi("features")) then
 		e.self:Say("Everquest Underground features two core ideas: the " .. eq.say_link("What underground?", false, "underground") .. " and " .. eq.say_link("Survival crafting?", false, "survival crafting") .. ".")
 	elseif(e.message:findi("underground")) then
